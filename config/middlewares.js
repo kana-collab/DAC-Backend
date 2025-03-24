84487e942ece826298557e4afec8d5cb356dd4af
kana-collab <zip5737@gmail.com>
2025-03-24 00:38:16 -0400

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://www.devallianceusa.com'], // Allow requests from your frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true, // If using cookies or authentication
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
